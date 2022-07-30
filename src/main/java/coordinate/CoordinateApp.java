e6d8a2945d6e880687c2c5fe11174e9330935474
eastshine-high <eastshine.high@gmail.com>
2022-07-30 22:54:27 +0900

    feat(coordinate): CoordinateContext 객체 생성 - 의존성 생성 및 주입을 담당

--- src/main/java/coordinate/CoordinateApp.java
+++ src/main/java/coordinate/CoordinateApp.java
@@ -1,20 +1,11 @@
 package coordinate;
 
+import coordinate.config.CoordinateContext;
 import coordinate.controller.CoordinateCalculator;
-import coordinate.model.FigureFactory;
-import coordinate.model.PointMapper;
-import coordinate.view.InputView;
-import coordinate.view.ResultView;
 
 public class CoordinateApp {
     public static void main(String[] args) {
-        CoordinateCalculator coordinateCaculator = new CoordinateCalculator(
-                new InputView(),
-                new ResultView(),
-                new PointMapper(),
-                new FigureFactory()
-        );
-
-        coordinateCaculator.run();
+        CoordinateCalculator coordinateCalculator = CoordinateContext.getCoordinateCalculator();
+        coordinateCalculator.run();
     }
 }
---
+++ src/main/java/coordinate/config/CoordinateContext.java
@@ -0,0 +1,27 @@
+package coordinate.config;
+
+import coordinate.controller.CoordinateCalculator;
+import coordinate.model.FigureFactory;
+import coordinate.view.InputView;
+import coordinate.view.ResultView;
+
+/**
+ * 의존성 생성과 주입을 담당
+ */
+public class CoordinateContext {
+    private static CoordinateCalculator coordinateCalculator;
+
+    private CoordinateContext() { }
+
+    public static synchronized CoordinateCalculator getCoordinateCalculator() {
+        if (coordinateCalculator == null) {
+            coordinateCalculator = new CoordinateCalculator(
+                    new InputView(),
+                    new ResultView(),
+                    new FigureFactory()
+            );
+        }
+
+        return coordinateCalculator;
+    }
+}
